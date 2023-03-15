b7872ecf8b20f0bdc503b0edb303d5fb9e7058e1
sibylassana95 <sibyamara95@gmail.com>
2023-03-15 18:54:04 +0000

    update

--- PasswordGenerator/src/password/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views 
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('passwords/', views.passwords, name='list_passwords'),
-    path('passwords_list/', views.passwords_list, name='passwords_list'),
-]
