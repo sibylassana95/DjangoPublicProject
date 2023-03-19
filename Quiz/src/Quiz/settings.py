00363494ef64e0290dd530b19a12cacfb0a3df26
sibylassana95 <sibyamara95@gmail.com>
2023-03-19 01:27:12 +0000

    update

--- Quiz/src/Quiz/settings.py
+++ Quiz/src/Quiz/settings.py
@@ -20,7 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#dg&upp8v&mwj!9y0rx#9xz#l&6hk52o#8$5-bi-&snqsq^ikd'
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
