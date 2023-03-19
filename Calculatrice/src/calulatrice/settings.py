06e2412232e7c1bfeeb679fc29e56c668150ec26
sibylassana95 <sibyamara95@gmail.com>
2023-03-19 00:58:10 +0000

    update

--- Calculatrice/src/calulatrice/settings.py
+++ Calculatrice/src/calulatrice/settings.py
@@ -20,7 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i@t651x8qg0pzqus#%us8p=979tm!)8p2jxas2@(1ne-g_jy7z'
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
