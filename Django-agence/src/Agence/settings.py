f40fa515597e9188621539714678f3c0e0b5c25b
sibylassana95 <sibyamara95@gmail.com>
2023-03-19 01:01:39 +0000

    update

--- Django-agence/src/Agence/settings.py
+++ Django-agence/src/Agence/settings.py
@@ -19,8 +19,11 @@ TEMPLATES_DIRS = os.path.join(BASE_DIR, "Agence/templates")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ec&a&3ne&-)muhrv9#_vx-x+a2kwv#jfz=m&wxyeu+2zmpcrr^'
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
