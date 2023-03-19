f71a078623d8dd477676f7f8c844a0de1bd8fd28
sibylassana95 <sibyamara95@gmail.com>
2023-03-19 00:48:54 +0000

    update

--- Administration/src/Application/settings.py
+++ Administration/src/Application/settings.py
@@ -11,16 +11,16 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
-
+from dotenv import load_dotenv, find_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIRS = os.path.join(BASE_DIR, "Application/templates")
-
+load_dotenv(find_dotenv())
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cou7_6a*ql5yf+h3(j#^$2n1lw$!@b^9z5ng2$#ryjhi&6bgvi'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
