0aaa56968bfeb49b93f3667ebfd7c953362f2323
sibylassana95 <sibyamara95@gmail.com>
2023-04-19 00:41:45 +0000

    update

--- Blog/src/blog/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Article
-
-class ArticleForm(forms.ModelForm):
-    class Meta:
-        model = Article
-        fields = ['titre', 'description', 'contenu', 'image']
